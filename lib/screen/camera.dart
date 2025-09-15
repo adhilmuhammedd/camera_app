c237c8a7d435ab7764736033b70410d742ac0273
adhilmuhammedd <mhdadhil1100@gmail.com>
2025-09-15 23:22:55 +0530

    Update camera.dart

--- lib/screen/camera.dart
+++ lib/screen/camera.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
-
+ 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
